94fdb78d83769c5d8461caba1d940f8e39d89bd7
Thomas Nguy <thomas.nguy@crypto.com>
2023-11-14 10:04:51 +0900

    prevent deposit base token using erc20 bridge

--- ethereum/contracts/dev-contracts/test/L1ERC20BridgeTest.sol
+++ ethereum/contracts/dev-contracts/test/L1ERC20BridgeTest.sol
@@ -6,7 +6,7 @@ import "../../bridge/L1ERC20Bridge.sol";
 
 /// @author Matter Labs
 contract L1ERC20BridgeTest is L1ERC20Bridge {
-    constructor(IZkSync _zkSync, IAllowList _allowList) L1ERC20Bridge(_zkSync, _allowList) {}
+    constructor(address _baseTokenAddress, IZkSync _zkSync, IAllowList _allowList) L1ERC20Bridge(_baseTokenAddress, _zkSync, _allowList) {}
 
     function getAllowList() public view returns (IAllowList) {
         return allowList;
